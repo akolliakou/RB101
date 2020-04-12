d3991a19200b75ab854a924d6d1641a9ca57050f
Anna Kolliakou <akolliakou@gmail.com>
2020-04-12 11:35:51 +0100

    Add exercises

---
+++ lesson-2/calculator.rb
@@ -0,0 +1,117 @@
+# ask the user for two numbers
+# ask the user for an operation to perform
+# perform the operation on the two numbers
+# output the result
+
+
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+LANGUAGE = 'en'
+
+def messages(message, lang='en')
+  MESSAGES[lang][message]
+end
+
+def prompt(key)
+  message = messages(key, LANGUAGE)
+  Kernel.puts("=> #{message}")
+end
+
+def valid_number?(number)
+  integer?(number) || float?(number)
+end
+
+def integer?(number)
+  number.to_i().to_s() == number
+end
+
+def float?(number)
+  number.to_f().to_s() == number
+end
+
+def operation_to_message(op)
+  word =case op
+        when '1'
+          (MESSAGES['addition'])
+        when '2'
+          (MESSAGES['subtraction'])
+        when '3'
+          (MESSAGES['multiplication'])
+        when '4'
+          (MESSAGES['division'])
+        end
+  x = "Checking that method value is still #{word}"
+
+  word
+end
+
+prompt('welcome')
+
+name = ''
+loop do
+  name = Kernel.gets().chomp()
+  break unless name == ''
+  prompt('valid_name')
+end
+
+prompt("Hello #{name}!")
+
+loop do # main loop
+  number1 = ''
+  loop do
+    prompt('first_number')
+    number1 = Kernel.gets().chomp()
+    break if valid_number?(number1)
+    prompt('invalid_first_number')
+  end
+
+  number2 = ''
+  loop do
+    prompt('second_number')
+    number2 = Kernel.gets().chomp()
+    break if valid_number?(number2)
+    prompt('invalid_second_number')
+  end
+
+  operator_prompt = <<~MSG
+  What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+
+  prompt(operator_prompt)
+
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt('operator_choice')
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  result = case operator
+           when '1'
+             number1.to_i() + number2.to_i()
+           when '2'
+             number1.to_i() - number2.to_i()
+           when '3'
+             number1.to_i() * number2.to_i()
+           when '4'
+             number1.to_f() / number2.to_f()
+           end
+
+  prompt("The result is #{result}")
+
+  prompt('more_operations')
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt('goodbye')
---
+++ lesson-2/mortgage_calculator.rb
@@ -0,0 +1,101 @@
+require 'yaml'
+MESSAGES = YAML.load_file('mortgage_messages.yml')
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def clear_screen
+  system('clear') || system('cls')
+end
+
+def valid_name?(string)
+  string != '' && string.to_i.to_s != string
+end
+
+def valid_number?(number)
+  integer?(number) || float?(number)
+end
+
+def integer?(number)
+  number.to_i.to_s == number && number.to_i > 0
+end
+
+def float?(number)
+  number.to_f.to_s == number && number.to_f > 0
+end
+
+def amount_final(amount)
+  amount.to_f.round(2)
+end
+
+def apr_monthly(apr)
+  apr = apr.to_f / 100 / 12
+  apr.round(4)
+end
+
+def months(months)
+  months.to_i * 12
+end
+
+def calculation(amount, apr, duration)
+  amount * (apr / (1 - (1 + apr)**(-duration)))
+end
+
+prompt(MESSAGES['welcome'])
+
+name = ''
+loop do
+  name = gets.chomp
+  break if valid_name?(name)
+  prompt(MESSAGES['valid_name'])
+end
+
+prompt("Hi, #{name}. Let's get started!")
+
+loop do
+  amt_input = ''
+  loop do
+    prompt(MESSAGES['amount'])
+    amt_input = gets.chomp
+    break if valid_number?(amt_input)
+    prompt(MESSAGES['invalid_amount'])
+  end
+
+  apr_input = ''
+  loop do
+    prompt(MESSAGES['apr'])
+    apr_input = gets.chomp
+    break if valid_number?(apr_input)
+    prompt(MESSAGES['invalid_apr'])
+  end
+
+  years_input = ''
+  loop do
+    prompt(MESSAGES['years'])
+    years_input = gets.chomp
+    break if valid_number?(years_input)
+    prompt(MESSAGES['invalid_years'])
+  end
+
+  monthly_payment = calculation(amount_final(amt_input), \
+                                apr_monthly(apr_input), \
+                                months(years_input))
+
+  summary = <<-SUM.strip
+   Thank you, #{name}!
+   For a loan amount of: £#{amount_final(amt_input)}
+   With a monthly: #{(apr_monthly(apr_input) * 100).to_s + '%'} APR
+   Over: #{months(years_input)} months
+   Your monthly repayment will be: £#{monthly_payment.round(2)}
+  SUM
+
+  prompt(summary)
+
+  prompt(MESSAGES['more_calculations'])
+  answer = gets.chomp
+  break unless answer.downcase == 'yes'
+  clear_screen
+end
+
+prompt(MESSAGES['goodbye'])
