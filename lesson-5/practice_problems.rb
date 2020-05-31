3761a93e282e6e4eb41af07a7acb1448c6d876af
Anna Kolliakou <akolliakou@gmail.com>
2020-05-31 14:42:15 +0100

    Add exercises

--- lesson-5/practice_problems.rb
+++ lesson-5/practice_problems.rb
@@ -225,6 +225,22 @@ end
 
 new_array
 
+# Problem 15
+# Given this data structure write some code to return an array 
+# which contains only the hashes where all the integers are even.
+
+# arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
+
+arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
+
+arr.select do |hsh|
+  hsh.all? do |k, v|
+    v.all? do |n|
+      n.odd?
+    end
+  end
+end
+
 # Problem 16
 # A UUID is a type of identifier often used as a way to uniquely identify items...
 # which may not all be created by the same system. That is, without any form of synchronization, 
