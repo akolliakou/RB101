61d8aa2ddadd78138894ec6c3fb1e1d51f4f0e66
Anna Kolliakou <akolliakou@gmail.com>
2020-10-01 16:34:47 +0100

    Cleaning repo

--- exercises/easy_6/cute_angles.rb
+++ exercises/easy_6/cute_angles.rb
@@ -21,13 +21,13 @@ def dms(n)
     new_array << seconds
   end
 
-  p  new_array.join
+  puts  new_array.join
 end
 
 
 # p dms(30) == %(30°00'00")
 # p dms(76.73) == %(76°43'48")
-dms(254.6) == %(254°36'00")
+p dms(254.6) == %(254°36'00")
 # p dms(93.034773) == %(93°02'05")
 # p dms(0) == %(0°00'00")
 # p dms(360) == %(360°00'00") || dms(360) == %(0°00'00")
